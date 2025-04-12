b3c2ecf885a2ee16c38cfa68e8bec8faff8ee4a4
AberQ <egor.master2017@gmail.com>
2025-04-12 22:17:45 +0500

    Добавил проверку ролей для post  /pvz

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, Request, HTTPException, status, Depends
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime, timedelta
 import asyncpg
 from asyncpg.pool import Pool
@@ -14,7 +14,7 @@ from schemas import *
 app = FastAPI()
 
 # JWT конфигурация
-SECRET_KEY = "0tyfd1TNrniHOvE8KwmLjyDTQ1x025K6hkZVQfVwvSE" 
+SECRET_KEY = "0tyfd1TNrniHOvE8KwmLjyDTQ1x025K6hkZVQfVwvSE"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -31,8 +31,6 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 # Глобальный пул подключений
 db_pool: Pool = None
-
-
 
 
 # Вставка пользователя в БД
@@ -56,14 +54,13 @@ async def register(user: UserCreate):
     return {"description": "Пользователь создан"}
 
 
-
-
 # Создание JWT токена
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
 
 @app.post("/dummyLogin")
 async def dummy_login(user: UserTypeRequest):
@@ -74,6 +71,19 @@ async def dummy_login(user: UserTypeRequest):
     return {"access_token": access_token, "token_type": "bearer"}
 
 
+# Получение роли из токена
+async def get_current_role(token: str = Depends(oauth2_scheme)) -> str:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        role: str = payload.get("sub")
+        if role is None:
+            raise HTTPException(status_code=401, detail="Недопустимый токен")
+        return role
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Не удалось проверить токен")
+
+
+# Вставка ПВЗ
 async def insert_pvz(city: str):
     try:
         async with db_pool.acquire() as conn:
@@ -82,11 +92,13 @@ async def insert_pvz(city: str):
         raise HTTPException(status_code=500, detail=f"Ошибка при добавлении ПВЗ: {str(e)}")
 
 
+# Добавление нового ПВЗ (только для модераторов)
 @app.post("/pvz", status_code=status.HTTP_201_CREATED)
-async def create_pvz(pvz: PVZCreate):
+async def create_pvz(pvz: PVZCreate, role: str = Depends(get_current_role)):
+    if role != "moderator":
+        raise HTTPException(status_code=403, detail="Доступ запрещён: требуется роль 'moderator'")
     await insert_pvz(pvz.city)
     return {"description": f"ПВЗ создан"}
-
 
 
 # Подключение к базе при старте
