6b00227b937eaf2a64b66c39dab04011e23309fe
AberQ <egor.master2017@gmail.com>
2025-04-12 21:33:50 +0500

    Добавил тестовое создание PVZ

--- init_db.py
+++ init_db.py
@@ -22,11 +22,10 @@ CREATE TABLE IF NOT EXISTS users (
 """
 
 CREATE_PVZ_TABLE_SQL = """
--- Включаем расширение для генерации UUID
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE IF NOT EXISTS PVZ_table (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id SERIAL PRIMARY KEY,
     registration_date TIMESTAMPTZ DEFAULT now(),
     city TEXT NOT NULL CHECK (city IN ('Москва', 'Санкт-Петербург', 'Казань'))
 );
@@ -62,7 +61,20 @@ async def create_user(email: str, password: str, role: str = 'employee'):
     except Exception as e:
         print("Ошибка при создании пользователя:", e)
 
+async def create_test_pvz(city: str = "Москва"):
+    try:
+        conn = await asyncpg.connect(**DB_CONFIG)
+
+        await conn.execute("""
+            INSERT INTO PVZ_table (city) VALUES ($1)
+        """, city)
+
+        print(f"Тестовая запись ПВЗ с городом '{city}' добавлена.")
+        await conn.close()
+    except Exception as e:
+        print("Ошибка при добавлении тестового ПВЗ:", e)
 
 if __name__ == "__main__":
     asyncio.run(create_db())
     asyncio.run(create_user("johndoe@example.com", "password123"))
+    asyncio.run(create_test_pvz())
